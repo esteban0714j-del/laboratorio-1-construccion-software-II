18a81ea130e00c428c070485a9c88155165e6308
Esteban_000J <esteban0714j@gmail.com>
2026-03-10 11:06:14 -0500

    Update Customer.java

--- ejercicio1-bancoajax/src/entity/Customer.java
+++ ejercicio1-bancoajax/src/entity/Customer.java
@@ -1,1 +1,54 @@
+package entity;
 
+public class Customer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public Customer() {}
+
+    public Customer(int id, String firstName, String lastName, double balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
