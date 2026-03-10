b472722419f7a1a69e345aa28e1324691174f1eb
Esteban_000J <esteban0714j@gmail.com>
2026-03-10 10:46:41 -0500

    Update ProductoFacade.java

--- ejercicio1/src/session/ProductoFacade.java
+++ ejercicio1/src/session/ProductoFacade.java
@@ -1,1 +1,18 @@
+package session;
 
+import entity.Producto;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class ProductoFacade {
+
+    @PersistenceContext(unitName = "restaurantePU")
+    private EntityManager em;
+
+    public List<Producto> findAll() {
+        return em.createQuery("SELECT p FROM Producto p").getResultList();
+    }
+}
